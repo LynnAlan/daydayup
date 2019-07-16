007979b6ec7fb3dbfa4b8ed19f081f7fdc6e3b9c
杨林 <yanglin001@zhongan.com>
2019-07-16 17:02:16 +0800

    test

--- 20190716.js
+++ 20190716.js
@@ -3,15 +3,13 @@
 //01 两数之和
 /**
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
-
   你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
 
   示例:
-
   给定 nums = [2, 7, 11, 15], target = 9
-
   因为 nums[0] + nums[1] = 2 + 7 = 9
   所以返回 [0, 1]
+
  *  
  */
 
@@ -45,21 +43,21 @@ function twoSum(nums,target){
  * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
 
   示例 1:
-
   输入: 123
   输出: 321
-  示例 2:
 
+  示例 2:
   输入: -123
   输出: -321
-  示例 3:
 
+  示例 3:
   输入: 120
   输出: 21
   注意:
 
   假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
  *  */
+// 方法一：转为数组然后处理
 function reverse(num){
   let numToArr = String(Math.abs(num)).split('');
   let reslut = '';
@@ -67,5 +65,24 @@ function reverse(num){
     reslut += numToArr.pop()
   }
   reslut = num > 0 ?  Number(reslut) : -Number(reslut) 
+  if (reslut > Math.pow(2, 31) - 1 || reslut < - Math.pow(2, 31)) {
+    reslut = 0;
+  }
+  return reslut
+}
+
+// 方法二 数学方式
+function reverse(num){
+  let reslut = 0
+  let t = Math.abs(num)
+  while( t != 0){
+    reslut = reslut * 10 + t % 10
+    console.log(reslut)
+    t = Math.floor(t / 10)
+  }
+  reslut = num > 0 ?  reslut : reslut
+  if (reslut > Math.pow(2, 31) - 1 || reslut < - Math.pow(2, 31)) {
+    reslut = 0;
+  }
   return reslut
 }
